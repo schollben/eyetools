d946d6a665ebf3f7563f888ad4c57484b4c897cd
Benjamin Scholl <scholl.ben@gmail.com>
2026-07-08 19:33:54 -0600

    fix

--- utils/create_subplot_grid.py
+++ utils/create_subplot_grid.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 
 def create_subplot_grid(n_sesh):
 
-    m = int(np.ceil(n_sesh / 2))
-    fig, axes = plt.subplots(2, m, 
-                             figsize=(2*m, 4), 
-                             sharex=False, 
+    n_cols = min(int(np.ceil(np.sqrt(n_sesh))), 4)
+    n_rows = int(np.ceil(n_sesh / n_cols))
+    fig, axes = plt.subplots(n_rows, n_cols,
+                             figsize=(2*n_cols, 2*n_rows),
+                             sharex=False,
                              squeeze=False)
     axes = axes.flatten()
+    for ax in axes[n_sesh:]:
+        ax.set_visible(False)
     sns.despine(fig=fig)
     fig.tight_layout(pad=2, h_pad=2, w_pad=2)
     return fig, axes

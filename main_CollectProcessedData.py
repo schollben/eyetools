d9215769d05d985b5c524cbb0d139c122e420b12
Benjamin Scholl <scholl.ben@gmail.com>
2026-07-08 19:31:27 -0600

    update create_subplot_grid

--- main_CollectProcessedData.py
+++ main_CollectProcessedData.py
@@ -27,11 +27,11 @@ plt.rcParams['svg.fonttype'] = 'none'
 
 # %% load data, delayed vision: 416,411,403
 #SESSION = getSesh.by_ferret(402, 420)    # multiple — preserves order by ferret
-SESSION = getSesh.by_ferret(420)          # or load sessions from an inidividual ID
+# SESSION = getSesh.by_ferret(420)          # or load sessions from an inidividual ID
 #SESSION = getSesh.by_name("session_2025-07-09_ferret_757_EyeCameras_P41_E13_analyzable_output") # or load a specific session by name
 #SESSION = getSesh.by_name("session_2026-03-16_ferret_403_P49_E7_analyzable_output") # or load a specific session by name
 #SESSION = getSesh.by_eo(7)      # or load sessions by a single EO number
-#SESSION = getSesh.by_eo(0, 4)   # or load sessions by an EO range (inclusive)
+SESSION = getSesh.by_eo(0, 4)   # or load sessions by an EO range (inclusive)
 
 Results = []
 for session in SESSION:
@@ -80,6 +80,7 @@ for n in range(n_sesh):
     axes[n].axis([1.5, 4.5, 0, 0.2])  # [xmin, xmax, ymin, ymax]
     axes[n].set_xticks([2, 3, 4])
     axes[n].set_yticks([0, 0.2])
+    axes[n].set_title(f"Ferret {Results[n].id} | EO {Results[n].eo}")
 
 
 # %% BASIC PLOTS: saccade rate distribution
